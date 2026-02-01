03ac38b7ad9c127a09e6d9207530c09172b765b1
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-01 13:41:29 +0200

    building two get methods and post method to get all tours/tour(id) and add new tour

--- app.js
+++ app.js
@@ -2,15 +2,54 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
-  fs.readFileSync(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((tour) => tour.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID ",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
-    "utf-8",
-    (err, data) => {},
-  ),
-);  
-app.get("/api/v1/tours", (req, res) => {});
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(` App is listining to port ${port} `);
