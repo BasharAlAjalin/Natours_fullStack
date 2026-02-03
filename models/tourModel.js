4cdcf3b34c0abf3800425342ac1aeac96cff3304
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-03 22:00:12 +0200

    Updating tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,15 +4,52 @@ const toursSchema = new mongoose.Schema({
     type: String,
     required: [true, " Tour must have a name"],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "Durations must be declared"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A size must be declared"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, " The difficultly must be declared"],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, " Tour must have a price"],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, " Summary must be declared"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, " cover image must be declared"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 const Tour = mongoose.model("Tour", toursSchema);
 module.exports = Tour;
