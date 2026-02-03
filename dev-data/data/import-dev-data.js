79ed72ef13dbd4364a4d1599d8a52c486061f657
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-03 23:12:07 +0200

    Writing a scipt to load the data from .json file

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const Tour = require("./../../models/tourModel");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log(" Connection done!"));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8"),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteOldDatabase = async () => {
+  try {
+    await Tour.deleteMany();
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteOldDatabase();
+}
