3e3f8cd85fdf74c4d696d3b7277abe1667957ce3
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-03 21:09:23 +0200

    Building Data model for Tours and create MVC architecure

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,62 +1,39 @@
-const fs = require("fs");
+const Tour = require("./../models/tourModel");
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
+// exports.getAllTours = (req, res) => {
+//   res.status(200).json({
+//     status: "success",
+//     // results: tours.length,
+//     // data: {
+//     //   tours,
+//     // },
+//   });
+// };
 
-exports.checkId = (req, res, next, val) => {
-  if (Number(req.params.id) > tours.length) {
-    return res.status(404).json({
+// exports.getTourById = (req, res) => {
+//   const id = Number(req.params.id);
+//   const tour = tours.find((tour) => tour.id === id);
+//   res.stats(200).json({
+//     status: "success",
+//     data: {
+//       tour,
+//     },
+//   });
+// };
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({
       status: "fail",
-      message: "Invalid ID",
+      message: "En error occured 👎",
     });
   }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.stats(400).json({
-      status: "fail",
-      message: "Missing name or price",
-    });
-  }
-  next();
-};
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-exports.getTourById = (req, res) => {
-  const id = Number(req.params.id);
-  const tour = tours.find((tour) => tour.id === id);
-  res.stats(200).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
-};
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    },
-  );
 };
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
 
-const port = 3000;
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+mongoose.connect(DB).then(() => console.log("connection successed"));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(` App is listining to port ${port} `);
 });
