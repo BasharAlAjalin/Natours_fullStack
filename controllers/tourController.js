0e45c4ec970af874a3ca6969d8296a958c5f3c04
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-05 12:42:21 +0200

    Building Pagination for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,6 +25,15 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select("-__v ");
     }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numberOfTours = await Tour.countDocuments();
+      if (skip >= numberOfTours) throw new Error("This page isn't found ");
+    }
     const tours = await query;
     res.status(200).json({
       status: "success",
