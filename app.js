944054da7b36f6ddb77e5f0fe11ba79a3ab16015
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-06 00:43:30 +0200

    Building an unknown routs error handler

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ app.set("query parser", "extended");
 app.use(express.static(`${__dirname}/public`));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+app.all(/.*/, (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Couldn't find  the ${req.originalUrl} on the server 👎`,
+  });
+});
 module.exports = app;
