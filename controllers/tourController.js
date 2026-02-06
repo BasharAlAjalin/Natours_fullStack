82122555056a905ad97ceb37c1df0d8fc8613445
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-06 11:21:59 +0200

    Buildin Error Handling for all types of error

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const AppError = require("../utils/appError");
 const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures");
 const catchAsync = require("./../utils/catchAsync");
@@ -20,6 +21,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTourById = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+  if (!tour) {
+    return next(new AppError("no tour found with this ID", 404));
+  }
   res.status(200).json({
     status: "success",
     data: {
@@ -43,6 +47,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  if (!newTour) {
+    return next(new AppError("no tour found with this ID", 404));
+  }
   res.status(201).json({
     status: "success",
     data: {
@@ -52,7 +59,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError("no tour found with this ID", 404));
+  }
   res.status(204).json({
     status: "success",
     data: null,
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception ");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD,
@@ -10,6 +15,14 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log("connection successed"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(` App is listining to port ${port} `);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection ");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
