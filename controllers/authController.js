c15fb1271184ad95676a556874cb83ff2f8b21cd
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-07 14:58:21 +0200

    Building login Funcionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,11 @@ const catchAsync = require("../utils/catchAsync");
 const User = require("./../models/userModel");
 const AppError = require("./../utils/appError");
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 exports.signup = catchAsync(async (req, res) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -11,9 +16,7 @@ exports.signup = catchAsync(async (req, res) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: "success",
     token,
@@ -30,10 +33,12 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   const user = await User.findOne({ email }).select("+password");
-  console.log(user);
-  const token = "";
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password!", 401));
+  }
+  const token = signToken(user._id);
   res.status(200).json({
     status: "success",
-    token,
+    token: token,
   });
 });
