d14a79e3cd6e584d3fd1770d6babaa1fc7f9dd2c
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-14 22:23:58 +0200

    Implement security layer with Helmet, MongoDB sanitization, HPP,and xss

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -9,6 +13,7 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+app.use(helmet());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -19,7 +24,10 @@ const limiter = rateLimit({
   message: "Too many request from the same IP , please try again in an hour ",
 });
 app.use("/api", limiter);
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
