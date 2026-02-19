c2392e303c4c2fa61ce798f5bd2908e8126056bc
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-19 15:20:26 +0200

    Building Nested Routes for reviews on tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,7 +4,9 @@ const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Review.find(), req.query)
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const features = new APIFeatures(Review.find(filter), req.query)
     .filter()
     .sort()
     .limitFields()
@@ -20,6 +22,8 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createNewReview = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
   const review = await Review.create(req.body);
   res.status(201).json({
     status: "success",
