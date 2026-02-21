49baf6c58a88b1f21203526b6d2ccad4f11aff93
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-21 22:14:12 +0200

    Calculating Average rating of tour reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -36,6 +37,40 @@ reviewSchema.pre(/^find/, function () {
   });
 });
 
+reviewSchema.statics.calcAvgRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+  console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: stats[0].nRating,
+      ratingsQuantity: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: 0,
+      ratingsQuantity: 4.5,
+    });
+  }
+};
+
+reviewSchema.post("save", function () {
+  this.constructor.calcAvgRatings(this.tour);
+});
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.review = await this.findOne();
+});
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.review.constructor.calcAvgRatings(this.review.tour);
+});
 const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
