016220c8ec31bfd6f89a03be64b02f38aaa53ab8
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-02-23 23:17:45 +0200

    Building a Pug file and building the skiliton of the app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
@@ -13,6 +14,9 @@ const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(helmet());
 if (process.env.NODE_ENV === "development") {
@@ -30,12 +34,19 @@ app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
 app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+app.get("/", (req, res) => {
+  res.status(200).render("base", {
+    tour: "The forest Hikers",
+    user: "Bisho",
+  });
+});
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
